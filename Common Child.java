17841b051ca3839230b31d6a3d6749df794f6461
Anand Wani <anand.wani1996@gmail.com>
2018-10-14 16:00:35 +0530

    Common Child
    A string is said to be a child of a another string if it can be formed by deleting 0 or more characters from the other string. Given two strings of equal length, what's the longest string that can be constructed such that it is a child of both?
    
    For example, ABCD and ABDC have two children with maximum length 3, ABC and ABD. They can be formed by eliminating either the D or C from both strings. Note that we will not consider ABCD as a common child because we can't rearrange characters and ABCD  ABDC.

---
+++ Common Child.java
@@ -0,0 +1,114 @@
+/*
+
+A string is said to be a child of a another string if it can be formed by deleting 0 or more characters from the other string. Given two strings of equal length, what's the longest string that can be constructed such that it is a child of both?
+
+For example, ABCD and ABDC have two children with maximum length 3, ABC and ABD. They can be formed by eliminating either the D or C from both strings. Note that we will not consider ABCD as a common child because we can't rearrange characters and ABCD  ABDC.
+
+Function Description
+
+Complete the commonChild function in the editor below. It should return the longest string which is a common child of the input strings.
+
+commonChild has the following parameter(s):
+
+s1, s2: two equal length strings
+Input Format
+
+There is one line with two space-separated strings,  and .
+
+Constraints
+
+All characters are upper case in the range ascii[A-Z].
+Output Format
+
+Print the length of the longest string , such that  is a child of both  and .
+
+Sample Input
+
+HARRY
+SALLY
+Sample Output
+
+ 2
+Explanation
+
+The longest string that can be formed by deleting zero or more characters from  and  is , whose length is 2.
+
+Sample Input 1
+
+AA
+BB
+Sample Output 1
+
+0
+Explanation 1
+
+ and  have no characters in common and hence the output is 0.
+
+Sample Input 2
+
+SHINCHAN
+NOHARAAA
+Sample Output 2
+
+3
+Explanation 2
+
+The longest string that can be formed between  and  while maintaining the order is .
+
+Sample Input 3
+
+ABCDEF
+FBDAMN
+Sample Output 3
+
+2
+Explanation 3 
+ is the longest child of the given strings.
+
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the commonChild function below.
+    static int commonChild(String s1, String s2) {
+        int array[][] = new int[s1.length()+1][s2.length()+1];
+        for(int i = 1;i < s1.length()+1;i++){
+            for(int j = 1;j < s2.length()+1;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    array[i][j] = array[i-1][j-1] + 1;
+                }else{
+                    array[i][j] = Math.max(array[i-1][j],array[i][j-1]);
+                }
+            }
+        }
+        return array[s1.length()][s2.length()];
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = scanner.nextLine();
+
+        String s2 = scanner.nextLine();
+
+        int result = commonChild(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
